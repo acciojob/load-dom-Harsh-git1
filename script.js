b4e874fd295778b5a7f8ac23a986fae075343627
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-02-03 00:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+	//callback
+	let para = document.createElement("p");
+	let textN = document.createTextNode("DOM load success");
+	para.appendChild(textN); // Append the text node to the paragraph element
+    document.body.appendChild(para)
+});
